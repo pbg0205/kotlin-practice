37fb202f80ce1d7778716d54d0d35b6fc3702fff
cooper <pbg0205@naver.com>
2022-07-11 20:55:00 +0900

    refactor: 이전 kotlin practice 제거

--- src/main/kotlin/com/cooper/practice/basic/list/ListPractice.kt
+++
@@ -1,8 +0,0 @@
-package com.cooper.practice.basic.list
-
-class ListPractice (private val list : List<Int>) {
-
-    fun getList() : List<Int> {
-        return list
-    }
-}
--- src/main/kotlin/com/cooper/practice/basic/nullable/Main.kt
+++
@@ -1,9 +0,0 @@
-package com.cooper.practice.basic.nullable
-
-fun main() {
-    val student1 = NullableStudent()
-    student1.printName()
-
-    val student2 = NullableStudent("cooper")
-    student2.printName()
-}
